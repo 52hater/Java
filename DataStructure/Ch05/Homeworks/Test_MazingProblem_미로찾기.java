6c472ccc889c499de790a2f095857fc78a867fac
52hater <youcancel@naver.com>
2024-03-25 01:08:19 +0900

    0324

--- DataStructure/Ch05/Homeworks/Test_MazingProblem_미로찾기.java
+++ DataStructure/Ch05/Homeworks/Test_MazingProblem_미로찾기.java
@@ -73,7 +73,6 @@ class StackList {
 			throw new OverflowIntStackException();
 		data.add(p);
 		top++;
-		return;
 	}
 
 	// --- 스택에서 데이터를 팝(정상에 있는 데이터를 꺼냄) ---//
@@ -116,63 +115,69 @@ public class Test_MazingProblem_미로찾기 {
 
 		mark[1][1] = 1; //울타리때문에 [1][1]
 		StackList st = new StackList(50);
-		Items3 temp = new Items3(0, 0, 0);//N :: 0//x y dir//temporary?
-		temp.x = 1; //울타리가 있으니 1로 초기화?
-		temp.y = 1;
+		Items3 temp = new Items3(0, 0, 0);//N :: 0//x y dir//temporary
+		temp.x = 1; //울타리가 있으니 1로 초기화? + 시작단계에서 !st.isEmpty()를 충족시키기위해 1,1로 초기화하는듯
+		temp.y = 1; //시작지점 y 좌표
 		temp.dir = 2;//E:: 2 ? east? / east 부터 탐색시작이라는 소리인듯? 그럼 NE가 1인가
+		//시작지점을 2로 마킹
 		mark[temp.x][temp.y] = 2;//미로 찾기 궤적은 2로 표시 // 경로표시지
+		//시작지점을 푸쉬
 		st.push(temp);
-		//temp 임시지점
+		//temp = 임시지점, 지금은 시작지점이지
 
-		while (!st.isEmpty() || ((temp.x == maze.length-1) && (temp.y == maze[0].length))) // 스택에 비어있지 않으면 루프(스택에 탐색할 위치가 남아있다) / 비면 빠져나와
-			
+		// 스택이 비어있지 않으면 루프(스택에 탐색할 위치가 남아있다) / 비면 빠져나와
+		// 갈데가 있으면 가서 스택을 쌓을텐데 그럼 스택이 빌때까지 루프돌아라는거잖아
+		// 근데 비는게 무슨소리인지? 결국 출구로 가도 출구까지 간 스택은 차있잖아
+		while (!st.isEmpty())
+
 		{
+			//팝 -> 이전위치로 돌아가지
 			Items3 tmp = st.pop(); // unstack// tmp 객체에 팝한걸 넣고
 			int i = tmp.x;
 			int j = tmp.y;
 			int d = tmp.dir;
-			int g = i + moves[d].a; //g, h 는 이전위치, i, j 는 이동 할 위치
-			int h = j + moves[d].b; //다음이동할위치의 y 좌표 ?
+			int g = i + moves[d].a; //g, h 는 새로운위치, i, j 는 현재위치 / 새로운위치로 이동
+			int h = j + moves[d].b; //다음이동할위치의 y 좌표 h
 
 			mark[i][j] = 1;//backtracking 궤적은 1로 표시
 			//현재위치를 backtracking 마킹한거
 			// 1, 2 헷갈리는데 개념이해를 아직 제대로 못한 듯
 			//>>백트레킹이니까 벽을 세우는거네 다시 못가도록
-			st.push(new Items3(i,j,d));
+
+
+			//이전위치를 스택에 푸쉬
+			//st.push(new Items3(i,j,d));
 
 			// moves forward
 			while (d < 8) {//이전지점이 있으니까 d < 8 / 가능한 모든 이동 dir 탐색
-				if ((g == ix) && (h == iy)) { // 이전위치가 다음위치 = 출구? 막혀? 
-					//왜 i 가 아니라 ix
-					mark[i][j] = 2;//2 마킹하면서 미로탈출
-					showMatrix(maze, i, j);
+				if (g >= 0 && g < maze.length && h >= 0 && h < maze[0].length) {
+					if ((maze[g][h] == 0) && (mark[g][h] == 0)) { // new position
+						//다음이동위치에 0이있고 마킹도 안돼있으면? 즉, 새로운 위치로 이동이 가능하면
+
+						//다음 이동방향 설정
+						temp = new Items3(0, 0, 0); //while 문 반복마다 새로운 객체사용하기위해// output path
+						mark[i][j] = 2; //이동한지점 마킹
+						st.push(temp);
+
+						i = g; // 이동할 새로운 위치 i 에 이전위치를 나타내는 g 대입
+						j = h; // 이동할 새로운 위치 j 에 이전위치를 나타내는 h 대입
+						d = 0; // 방향초기화
+					}
+					if ((g == ix) && (h == iy)) { //다음이동위치가 출구면
+						//path 메소드 호출하는데서 12, 15를 줬으니 ix, iy 가 나타내는것은 출구의 좌표
+						mark[i][j] = 2;//2 마킹하면서 미로탈출
+						showMatrix(maze, i, j);
+						System.out.println("출구찾음");
+						break;
+					}
+					else
+						temp = new Items3(0, 0, 0);
+					d++; //북쪽으로 못가면 d++로 북동쪽(?) / move 배열의 방향
+					continue;
 				}
-
-				if ((maze[g][h] == 0) && (mark[g][h] == 0)) { // new position
-					//현재위치에 0이있고 마킹도 안돼있으면?
-					temp = new Items3(0, 0, 0); //while문 반복마다 새로운 객체사용하기위해// output path
-					temp.x = i;
-					temp.y = j;
-					temp.dir = d+1; //다음 이동 dir 설정 ex) E + 1 = SE
-
-					g = i + moves[d].a;
-					h = j + moves[d].b;
-
-					mark[i][j] = 2; //이동한지점 마킹
-					st.push(temp);
-
-					i = g; // 이동할 새로운 위치 i 에 이전위치를 나타내는 g 대입
-					j = h; // 이동할 새로운 위치 j 에 이전위치를 나타내는 h 대입
-					d = 0; // 방향초기화
-				} else
-					temp = new Items3(0, 0, 0);
-					d++; //북쪽으로 못가면 d++로 북동쪽(?) / move 배열에 방향
-				g = i + moves[d].a;
-				h = j + moves[d].b;
-
-				mark[i][j] = 2;
-				st.push(temp);
-			}
+				st.pop();
+				mark[i][j] = 1;
+			}     
 		}
 		System.out.println("no path in maze "); //스택이 비면
 	}
@@ -209,10 +214,13 @@ public class Test_MazingProblem_미로찾기 {
 
 		for (int ia = 0; ia < 8; ia++)
 			moves[ia] = new Offsets3(0, 0);//배열에 offsets 객체를 치환해야 한다.
+		//moves[ib]를 따로 안해도 어차피 객체만 할당하고 초기화하니까 moves[ib]까지 작동(?)이 되는건지??
+
 		//moves 배열 : 8방향 0~7 저장
 		//ia가 moves 배열에 새로운 Offsets3 객체를 할당하는데 사용되는 반복인덱스
-		moves[0].a = -1;	moves[0].b = 0;
-		moves[1].a = -1;	moves[1].b = 1;
+		//d에 대한 이동방향정보
+		moves[0].a = -1;	moves[0].b = 0; //N -> 북쪽으로 한칸이니까 row-1 col 은 그대로
+		moves[1].a = -1;	moves[1].b = 1; //NE ->북동쪽으로 한칸이니까 row-1 col+1
 		moves[2].a = 0;		moves[2].b = 1;
 		moves[3].a = 1;		moves[3].b = 1;
 		moves[4].a = 1;		moves[4].b = 0;
--- DataStructure/Ch06/Homeworks/Test_객체merge정렬.java
+++ DataStructure/Ch06/Homeworks/Test_객체merge정렬.java
@@ -3,19 +3,55 @@ package DataStructure.Ch06.Homeworks;
  * 6장 구현 실습과제1 
  */
 class PhyscData implements Comparable<PhyscData>{
-    String name;              // 이름
-    int    height;            // 키
-    double vision;            // 시력
+	String name;              // 이름
+	int    height;            // 키
+	double vision;            // 시력
 
+	public PhyscData(String name, int height, double vision) {
+		super();
+		this.name = name;
+		this.height = height;
+		this.vision = vision;
+	}
 
+	@Override
+	public int compareTo(PhyscData p) {
+		// TODO Auto-generated method stub
+		return this.name.compareTo(p.name);
+		//return this.height - p.height;//현재객체 this의 키에서 비교객체키를 빼서 비교
+	}
 
 }
+
+
+
 public class Test_객체merge정렬 {
 	// --- 배열 요소 a[idx1]와 a[idx2]의 값을 교환 ---//
 	static void merge(PhyscData[] a, int lefta, int righta, int leftb, int rightb ) {
+		//두개로 갈라진 배열 합쳐지는 임시배열
+		//PhyscData[] temp = new PhyscData[(righta+1) + (rightb+1)]; //length 니까 +1 //이거 왜 안돼?
+		PhyscData[] temp = new PhyscData[(righta-lefta+1) + (rightb-leftb+1)]; //length 니까 +1
+		
+		int tempI = 0;
 
-
+		while (lefta <= righta && leftb <= rightb) {
+			if (a[lefta].compareTo(a[leftb]) <= 0) {
+				temp[tempI++] = a[lefta++];
+			} else {
+				temp[tempI++] = a[leftb++];
+			}
+		}
+		while (lefta <= righta) {
+			temp[tempI++] = a[lefta++];
+		}
+		while (leftb <= rightb) {
+			temp[tempI++] = a[leftb++];
+		}
+		for (int i = 0; i < temp.length; i++ ) {
+			a[i] = temp[i];
+		}
 	}
+	
 
 	// --- 퀵 정렬(비재귀 버전)---//
 	static void MergeSort(PhyscData[] a, int left, int right) {
@@ -27,24 +63,29 @@ public class Test_객체merge정렬 {
 		return;
 	}
 
+
 	public static void main(String[] args) {
+
 		PhyscData[] x = {
-		         new PhyscData("강민하", 162, 0.3),
-		         new PhyscData("김찬우", 173, 0.7),
-		         new PhyscData("박준서", 171, 2.0),
-		         new PhyscData("유서범", 171, 1.5),
-		         new PhyscData("이수연", 168, 0.4),
-		         new PhyscData("장경오", 171, 1.2),
-		         new PhyscData("황지안", 169, 0.8),
-		     };
+				new PhyscData("이수연", 168, 0.4),
+				new PhyscData("유서범", 171, 1.5),
+				new PhyscData("김찬우", 173, 0.7),
+				new PhyscData("황지안", 169, 0.8),
+				new PhyscData("강민하", 162, 0.3),
+				new PhyscData("박준서", 171, 2.0),
+				new PhyscData("유서범", 171, 1.5),
+				new PhyscData("장경오", 171, 1.2),
+
+		};
+
 		int nx = x.length;
 
-		   MergeSort(x, 0, nx - 1); // 배열 x를 퀵정렬
-			System.out.println("오름차순으로 정렬했습니다.");
-		     System.out.println("■ 신체검사 리스트 ■");
-		     System.out.println(" 이름     키  시력");
-		     System.out.println("------------------");
-		     for (int i = 0; i < x.length; i++)
-		         System.out.printf("%-8s%3d%5.1f\n", x[i].name, x[i].height, x[i].vision);
+		MergeSort(x, 0, nx - 1); // 배열 x를 퀵정렬
+		System.out.println("오름차순으로 정렬했습니다.");
+		System.out.println("■ 신체검사 리스트 ■");
+		System.out.println(" 이름     키  시력");
+		System.out.println("------------------");
+		for (int i = 0; i < x.length; i++)
+			System.out.printf("%-8s%3d%5.1f\n", x[i].name, x[i].height, x[i].vision);
 	}
 }
